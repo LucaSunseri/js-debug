5db15c926be325326b7a9cd81620088cb9049ef5
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-12-09 17:17:33 +0100

    First commit

---
+++ bundle-01/main.js
@@ -0,0 +1,100 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+
+// ESERCIZIO 1
+for (let i = 0; i > 5; i++) {
+    console.log(i);
+}
+
+/* 
+    1- Che cosa fa questo codice?
+        Il codice esegue un ciclo for
+    2- Sono presenti errori di sintassi?
+        Non sono presenti erroti di sintassi
+    3- Sono presenti errori logici?
+        Si perchè in questo caso la condizione inserita non sarà mai valida ed il ciclo non inizierà mai
+*/
+
+
+// ESERCIZIO 2
+/*
+!FUNZIONE ERRATA
+function addIfEven(num) {
+    if (num % 2 = 0) {
+        return num + 5;
+    }
+    return num;
+}
+*/
+
+/* 
+    1- Che cosa fa questo codice?
+        La funzione aggiunge +5 se il numero è pari
+    2- Sono presenti errori di sintassi?
+        Si l'uguale, perchè un solo uguale è assegnazione
+    3- Sono presenti errori logici?
+        No
+*/
+
+// *FUNZIONE CORRETTA
+function addIfEven(num) {
+    if (num % 2 == 0) {
+        return num + 5;
+    }
+    return num;
+}
+
+
+// // ESERCIZIO 3
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
+
+
+// ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+/* 
+!FUNZIONE ERRATA
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+        return evenNumbers;
+    }
+}
+*/
+
+/* 
+    1- Che cosa fa questo codice?
+        Restiusci un array di numeri pari
+    2- Sono presenti errori di sintassi?
+        Si
+    3- Sono presenti errori logici?
+        
+*/
+
+// *FUNZIONE CORRETTA
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 == 0) {
+            evenNumbers.push(numbers[i]);
+        }
+    }
+    return evenNumbers;
+}
+displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
---
+++ bundle-02/main.js
@@ -0,0 +1,223 @@
+/*******************************************************************************
+​
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+​
+    Inserisci i tuoi commenti con le risposte alla fine dell'esercizio
+​
+*******************************************************************************/
+
+
+// ESERCIZIO 1
+/*
+!FUNZIONE ERRATA
+function checkAge() {
+    const myAge = 32;
+    const message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+}
+checkAge();
+*/
+
+/* 
+    1- Che cosa fa questo codice?
+        La funzione controlla l'età
+    2- Sono presenti errori di sintassi?
+        Si, non possiamo assegnare una costante ad una variabile
+    3- Sono presenti errori logici?
+        No
+*/
+
+// *FUNZIONE CORRETTA
+function checkAge() {
+    const myAge = 32;
+    let message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+}
+// checkAge();
+
+// ESERCIZIO 2
+/*
+!FUNZIONE ERRATA
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+}
+printColorsNumber();
+*/
+
+/* 
+    1- Che cosa fa questo codice?
+        La funzione mi dice quanti colori sono presenti nel mio array 
+    2- Sono presenti errori di sintassi?
+        Si, la parola 'lenght' scritta in modo sbagliato 
+    3- Sono presenti errori logici?
+        No
+*/
+
+// *FUNZIONE CORRETTA
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+// printColorsNumber();
+
+
+// ESERCIZIO 3
+/*
+!FUNZIONE ERRATA
+function addNumbers() {
+    const userNumber = prompt('Inserisci un numero');
+    const total = userNumber + 12;
+
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
+*/
+
+/* 
+    1- Che cosa fa questo codice?
+        La funzione aggiunge un numero al numero inserito dall'utente
+    2- Sono presenti errori di sintassi?
+        Si, non possiamo sommare un numero ad una stringa
+    3- Sono presenti errori logici?
+        No
+*/
+
+// *FUNZIONE CORRETTA
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    const total = userNumber + 12;
+
+    console.log(`Il risultato finale è ${total}`);
+}
+// addNumbers();
+
+
+// ESERCIZIO 4
+/*
+!FUNZIONE ERRATA
+function checkAccess() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = 'false';
+
+    if (addresses.includes(userEmail)) {
+        grantAccess = 'true';
+    }
+
+    if (grantAccess === true) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccess();
+*/
+
+/* 
+    1- Che cosa fa questo codice?
+        La funzione controlla se l'email inserita è presente nell'array di email
+    2- Sono presenti errori di sintassi?
+        Si, o false e true li utilizziamo come variabile booleana e non come stringa o viceversa, utilizziamo tutto come stringa
+    3- Sono presenti errori logici?
+        No
+*/
+
+// *FUNZIONE CORRETTA
+function checkAccess() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    if (addresses.includes(userEmail)) {
+        grantAccess = true;
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+// checkAccess();
+
+
+// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+/*
+!FUNZIONE ERRATA
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    for (let i = 0; i < addresses.length; i++) {
+     const email = addresses[i];
+        
+      if (userEmail.length > 5) {
+            
+        if (email === userEmail) {
+        grantAccess = 'true';            
+            
+      }
+        
+    }
+
+    if (grantAccess) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccessImproved();
+*/
+
+/* 
+    1- Che cosa fa questo codice?
+        La funzione controlla se l'email inserita è presente nell'array di email, il controllo avviene solo se la lunghezza inserita è maggiore di 5 
+    2- Sono presenti errori di sintassi?
+        Si, mancava una parentesi alla fine
+    3- Sono presenti errori logici?
+        Si
+*/
+
+// *FUNZIONE CORRETTA
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    for (let i = 0; i < addresses.length; i++) {
+     const email = addresses[i];
+        
+        if (userEmail.length > 5) {
+                
+            if (email === userEmail) {
+            grantAccess = true;                
+            }
+   
+        }
+    }
+    
+    if (grantAccess) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+// checkAccessImproved();
