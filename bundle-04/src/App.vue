e712d5b59ca82394c004355b76955a33b583adc0
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-12-09 18:16:32 +0100

    Bundle-04 Done

---
+++ bundle-04/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="app">
+        <Header
+            :genres="genresList"
+            :authors="authorsList"
+            @changedGenere="startGenreSearch"
+            @changedAuthor="startAuthorSearch"
+        />
+        <!-- L'emit era scritto in modo sbagliato -->
+
+        <Main
+            :selectedGenre="genreToSearch"
+            :selectedAuthor="authorToSearch"
+            @genresAndAuthorsReady="getGenresAndAuthorsList"
+        />
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+export default {
+    name: "App",
+    components: {
+        Header,
+        Main,
+    },
+    data() {
+        return {
+            genresList: [],
+            authorsList: [],
+            genreToSearch: "",
+            authorToSearch: "",
+        };
+    },
+    methods: {
+        getGenresAndAuthorsList(allGenresAndAuthors) {
+            // !allGenresAndAuthors = {}; perchè? resettare
+            this.genresList = allGenresAndAuthors.genres;
+            this.authorsList = allGenresAndAuthors.authors;
+        },
+        startGenreSearch(genreToSearch) {
+            console.log(genreToSearch);
+            // !genreToSearch = ""; perchè? resettare
+            this.genreToSearch = genreToSearch;
+        },
+        startAuthorSearch(authorToSearch) {
+            console.log(authorToSearch);
+            this.authorToSearch = authorToSearch;
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/styles/general";
+</style>
---
+++ bundle-04/src/components/Disc.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="disc p-3 text-center">
+        <div class="p-3">
+            <img class="img-fluid mb-3" :src="disc.poster" :alt="disc.name" />
+            <!-- discs invece di disc.poster  -->
+            <p class="title text-uppercase fw-bold">{{ disc.title }}</p>
+            <p class="author lead mb-0">{{ disc.author }}</p>
+            <!-- mancano le doppie parentesi graffe -->
+            <p class="year">{{ disc.year }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Disc",
+    props: {
+        // !disc: String, non passiamo una stringa ma un oggetto
+        disc: Object,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.disc {
+    flex-basis: calc(100% / 5);
+
+    .author,
+    .year {
+        color: #7d7977;
+    }
+}
+
+div {
+    background-color: #2e3a46;
+    height: 100%;
+}
+</style>
